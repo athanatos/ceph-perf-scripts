5f2a3d2be80368b4cad6a45a3d231c3078981c5a
Samuel Just <sjust@redhat.com>
2016-02-05 15:18:42 -0800

    working

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIRECTORY = ~/sjust/output/$1-$(date '+%Y-%m-%d-%H:%M:%S')
+mkdir $DIRECTORY
+for i in 64 32 128 5G 10G
+do
+        rm -rf /mnt/filestore/*
+        mkdir $DIRECTORY/$i
+        ./run_smalliobench.py  --smalliobench-path $2 --journal-path /dev/disk/by-partlabel/journal-$i --config conf.yaml --filestore-path /mnt/filestore --output-path $DIRECTORY/$i
+done
