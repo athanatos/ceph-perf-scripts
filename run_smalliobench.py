96ffd83867ae11aa299aa85bda5f7aad9bbdeded
Samuel Just <sjust@redhat.com>
2016-02-05 14:22:39 -0800

    working

--- run_smalliobench.py
+++ run_smalliobench.py
@@ -99,11 +99,11 @@ def process_log_file(fd):
             current = []
     def project(ind, l):
         return [x[ind] for x in l]
-    99_latencies = np.array(project(2, ps))
+    nn_latencies = np.array(project(2, ps))
     tpt = np.array(project(3, ps))
     return {
-        '99_latency_stddev_micro': np.std(99_latencies) * (10**6)
-        '99_latency_avg_micro': np.mean(99_latencies) * (10**6)
+        '99_latency_stddev_micro': np.std(nn_latencies) * (10**6)
+        '99_latency_avg_micro': np.mean(nn_latencies) * (10**6)
         'avg_latency_micro': np.mean(project(1, ps)) * (10**6)
         'throughput_stddev': np.std(tpt)
         'throughput_avg': np.mean(tpt)
