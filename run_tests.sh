0b7b3ce970a607deb30666f7782d269234705897
Samuel Just <sjust@redhat.com>
2016-02-15 13:42:26 -0800

    working

--- run_tests.sh
+++ run_tests.sh
@@ -3,9 +3,9 @@
 DIRECTORY=~/sjust/output/$1-$(date '+%Y-%m-%d-%H:%M:%S')
 echo $DIRECTORY
 mkdir $DIRECTORY
-for i in 10G
+for i in 32 10G
 do
         rm -rf /mnt/filestore/*
         mkdir $DIRECTORY/$i
-        ./run_smalliobench.py  --smalliobench-path $2 --journal-path /dev/disk/by-partlabel/journal-$i --config conf4.yaml --filestore-path /mnt/filestore --output-path $DIRECTORY/$i
+        ./run_smalliobench.py  --smalliobench-path $2 --journal-path /dev/disk/by-partlabel/journal-$i --config conf5.yaml --filestore-path /mnt/filestore --output-path $DIRECTORY/$i
 done
