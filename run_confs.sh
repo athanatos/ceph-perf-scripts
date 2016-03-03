a5876c11adc5645176cdc0b86ca4387431316609
Samuel Just <sjust@redhat.com>
2016-03-03 14:44:23 -0800

    working

--- run_confs.sh
+++ run_confs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DIRECTORY=~/sjust/output/$1-$(date '+%Y-%m-%d-%H:%M:%S')
+DIRECTORY=~/output/$1-$(date '+%Y-%m-%d-%H:%M:%S')
 echo $DIRECTORY
 mkdir $DIRECTORY
 for i in $(ls confs)
 do
         rm -rf /mnt/filestore/*
         mkdir $DIRECTORY/$i
-        ./run_smalliobench.py  --smalliobench-path $2 --journal-path /dev/disk/by-partlabel/journal-64 --config confs/$i --filestore-path /mnt/filestore --output-path $DIRECTORY/$i
+        ./run_smalliobench.py  --smalliobench-path $2 --journal-path /dev/sdb1 --config confs/$i --filestore-path /mnt/filestore --output-path $DIRECTORY/$i
 done
